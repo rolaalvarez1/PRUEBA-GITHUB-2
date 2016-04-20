b2162abaec3e6c35f9398d4993be0e0c04d52053
enzoxs <enzo_002@hotmail.com>
2016-04-20 08:16:15 -0400

    Primer Commit
    mi primer commit :)

---
+++ README.md
@@ -0,0 +1,9 @@
+# Esto es una prueba de uso de Github
+
+
+Segunda Prueba de uso de GitHub
+
+[Visita mi Twitter](http://twitter.com/enzoxs)
+
+
+
---
+++ prueba 2.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int f;
+
+main()
+{
+	do{
+printf("ingrese un numero 2");
+scanf("%d",f);
+	}while (f==2)
+}
